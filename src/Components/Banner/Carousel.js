d7d5d2683864ff722a29f989d6e13038c6be244f
Arshu <arshu.idstats@gmail.com>
2023-05-08 16:14:04 +0530

    change 1.1v

--- src/Components/Banner/Carousel.js
+++ src/Components/Banner/Carousel.js
@@ -13,11 +13,11 @@ const Carousel = () => {
   const { currency, symbol } = CryptoState();
   const fetchtendingcoins = async () => {
     const { data } = await axios.get(TrendingCoins(currency));
-    const response = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h"
-    );
-    const val = await response.json();
-    console.log(val);
+    // const response = await fetch(
+    //   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h"
+    // );
+    // const val = await response.json();
+    // console.log(val);
     setTrending(data);
   };
 
